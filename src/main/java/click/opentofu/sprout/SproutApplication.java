384b3381a3ee7c4a3834340df03292d8223a42d6
mole <hyunmin.lim.90@icloud.com>
2026-03-11 15:58:30 +0900

    GITLAB 마이그레이션 2026

---
+++ src/main/java/click/opentofu/sprout/SproutApplication.java
@@ -0,0 +1,13 @@
+package click.opentofu.sprout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SproutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SproutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/click/opentofu/sprout/SproutApplicationTests.java
@@ -0,0 +1,13 @@
+package click.opentofu.sprout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SproutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
